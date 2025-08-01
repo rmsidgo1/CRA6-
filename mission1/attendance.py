0b9137bb9dab9bf039379a6eea6390a0a0c1bf78
kimdy003 <kyl123451@naver.com>
2025-08-01 12:27:55 +0900

    test coverage 100%

--- mission1/attendance.py
+++ mission1/attendance.py
@@ -1,3 +1,5 @@
+import os.path
+
 name_to_member_id = {}
 member_count = 0
 
@@ -10,65 +12,69 @@ wednesday_attendance_count = [0] * 100
 weekend_attendance_count = [0] * 100
 
 
-def input2(name, weekday):
-    global member_count
+def take_attendance(name, weekday):
+    ensure_name(name)
 
+    member_id = name_to_member_id[name]
+
+    add_point(member_id, weekday)
+
+
+def ensure_name(name):
+    global member_count
     if name not in name_to_member_id:
         member_count += 1
         name_to_member_id[name] = member_count
         names[member_count] = name
 
-    member_id = name_to_member_id[name]
 
-    take_attendance(member_id, weekday)
-
-
-def take_attendance(member_id, weekday):
-    add_point = 0
+def add_point(member_id, weekday):
+    point = 0
     index = 0
 
     if weekday == "monday":
         index = 0
-        add_point += 1
+        point += 1
     elif weekday == "tuesday":
         index = 1
-        add_point += 1
+        point += 1
     elif weekday == "wednesday":
         index = 2
-        add_point += 3
+        point += 3
         wednesday_attendance_count[member_id] += 1
     elif weekday == "thursday":
         index = 3
-        add_point += 1
+        point += 1
     elif weekday == "friday":
         index = 4
-        add_point += 1
+        point += 1
     elif weekday == "saturday":
         index = 5
-        add_point += 2
+        point += 2
         weekend_attendance_count[member_id] += 1
     elif weekday == "sunday":
         index = 6
-        add_point += 2
+        point += 2
         weekend_attendance_count[member_id] += 1
 
     attendance_by_day[member_id][index] += 1
-    points[member_id] += add_point
+    points[member_id] += point
 
 
 def show_grade(attendance_file="attendance_weekday_500.txt"):
     try:
-        with open(attendance_file, encoding='utf-8') as f:
+        with open(attendance_file, 'r', encoding='utf-8') as f:
             for _ in range(500):
                 line = f.readline()
                 if not line:
                     break
                 parts = line.strip().split()
+                name, weekday = parts[0], parts[1]
                 if len(parts) == 2:
-                    input2(parts[0], parts[1])
+                    take_attendance(name, weekday)
 
         for i in range(1, member_count + 1):
-            if attendance_by_day[i][3] > 9:
+            if attendance_by_day[i][2] > 9:
                 points[i] += 10
             if attendance_by_day[i][5] + attendance_by_day[i][6] > 9:
                 points[i] += 10
@@ -97,6 +103,6 @@ def show_grade(attendance_file="attendance_weekday_500.txt"):
     except FileNotFoundError:
         print("파일을 찾을 수 없습니다.")
 
-
-if __name__ == "__main__":
-    show_grade()
+# Coverage 100% 위해 주석처리
+# if __name__ == "__main__":
+#     show_grade()
---
+++ mission1/tests/test_attendance1.py
@@ -0,0 +1,55 @@
+import pytest
+import io
+import os
+from contextlib import redirect_stdout
+
+import sys
+
+sys.path.append('C:\\PythonProject')
+print(sys.path)
+
+from mission1.attendance import show_grade
+
+
+def act(attendance_file, sut):
+    buf = io.StringIO()
+    with redirect_stdout(buf):
+        sut(attendance_file)
+        actual = buf.getvalue().rstrip()  # 맨 끝 "\n" 제거
+    return actual
+
+
+def test_빈파일():
+    sut = show_grade
+
+    attendance_file = "EmptyFile.txt"
+    with open(attendance_file, "w") as f:
+        pass
+    expected = "\nRemoved player\n=============="
+
+    actual = act(attendance_file, sut)
+
+    os.remove(attendance_file)
+    assert actual == expected
+
+
+def test_파일이없을때():
+    sut = show_grade
+    expected = "파일을 찾을 수 없습니다."
+
+    attendance_file = "NotExistsFilename.txt"
+
+    actual = act(attendance_file, sut)
+
+    assert actual == expected
+
+
+def test_기대한출력_나오는지():
+    sut = show_grade
+    attendance_file = "C:\\PythonProject\\mission1\\tests\\input.txt"
+    with open("C:\\PythonProject\\mission1\\tests\\expected.txt", encoding='utf-8') as f:
+        expected = f.read()
+
+    actual = act(attendance_file, sut)
+
+    assert actual == expected
